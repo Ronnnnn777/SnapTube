912db63ec7bd5c7c50ead319673370df438b3ff3
Aaron Angeles <aaronleeangeles17@gmail.com>
2025-11-15 15:25:12 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SnapTube',
+  webDir: 'www'
+};
+
+export default config;
